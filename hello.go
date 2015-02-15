6f2bb33858c02af28a2dc5c4bed7bc5a4877e9aa
jleo3 <joseph.leo3@gmail.com>
2015-02-15 11:40:21 -0500

    use a package

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jleo3/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, Joe.\n")
+	fmt.Printf(stringutil.Reverse("Hello, Joe.\n"))
 }
+
